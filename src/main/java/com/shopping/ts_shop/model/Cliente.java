408f04521b731164ea6fcf05d82045bd7c177bbe
tiagosantos-dev <tiagoltj@hotmail.com>
2025-02-03 19:11:18 -0300

    feat(Validacao): Adicionando validação na entidade cliente

--- src/main/java/com/shopping/ts_shop/model/Cliente.java
+++ src/main/java/com/shopping/ts_shop/model/Cliente.java
@@ -1,6 +1,10 @@
 package com.shopping.ts_shop.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Entity
@@ -10,12 +14,19 @@ public class Cliente {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "O nome não pode estar vazio")
+    @Size(min = 2, max = 255, message = "O nome deve ter entre 2 e 255 caracteres")
     private String nome;
 
+    @NotBlank(message = "O e-mail é obrigatório")
+    @Email(message = "E-mail inválido")
     @Column(unique = true, nullable = false)
     private String email;
 
+    @NotBlank(message = "O telefone é obrigatório")
+    @Pattern(regexp = "\\d{10,15}", message = "O telefone deve conter entre 10 e 15 dígitos numéricos")
     private String telefone;
 
+    @NotBlank(message = "O endereço não pode estar vazio")
     private String endereco;
 }
