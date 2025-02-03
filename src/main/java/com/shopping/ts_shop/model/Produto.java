2ce7cb20eea78984bbfbd3bb68fabdccd0b20a5b
tiagosantos-dev <tiagoltj@hotmail.com>
2025-02-03 19:12:28 -0300

    feat(Validacao): Adicionando validação na entidade produto

--- src/main/java/com/shopping/ts_shop/model/Produto.java
+++ src/main/java/com/shopping/ts_shop/model/Produto.java
@@ -1,6 +1,10 @@
 package com.shopping.ts_shop.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import java.math.BigDecimal;
 
@@ -11,12 +15,17 @@ public class Produto {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "O nome do produto é obrigatório")
     private String nome;
     private String descricao;
 
+    @NotNull(message = "O preço é obrigatório")
+    @DecimalMin(value = "0.01", message = "O preço deve ser maior que zero")
     @Column(nullable = false)
     private BigDecimal preco;
 
+    @NotNull(message = "O estoque é obrigatório")
+    @Min(value = 0, message = "O estoque não pode ser negativo")
     @Column(nullable = false)
     private Integer estoque;
 }
