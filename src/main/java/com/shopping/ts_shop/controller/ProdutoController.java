45fc02e5e5c8a6d74e50fcf880b1bdf7a1191904
tiagosantos-dev <tiagoltj@hotmail.com>
2025-02-03 20:09:43 -0300

    feat(Produto): Criando endpoint para o produto

---
+++ src/main/java/com/shopping/ts_shop/controller/ProdutoController.java
@@ -0,0 +1,48 @@
+package com.shopping.ts_shop.controller;
+
+import com.shopping.ts_shop.model.Produto;
+import com.shopping.ts_shop.service.ProdutoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import jakarta.validation.Valid;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/produtos")
+@Validated
+public class ProdutoController {
+    @Autowired
+    private  ProdutoService produtoService;
+
+
+    @GetMapping
+    public ResponseEntity<List<Produto>> listarTodos() {
+        return ResponseEntity.ok(produtoService.listarTodos());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Produto> buscarPorId(@PathVariable Long id) {
+        return produtoService.buscarPorId(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<Produto> criar(@Valid @RequestBody Produto produto) {
+        return ResponseEntity.ok(produtoService.criar(produto));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Produto> atualizar(@PathVariable Long id, @Valid @RequestBody Produto produto) {
+        return ResponseEntity.ok(produtoService.atualizar(id, produto));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> excluir(@PathVariable Long id) {
+        produtoService.excluir(id);
+        return ResponseEntity.noContent().build();
+    }
+}
