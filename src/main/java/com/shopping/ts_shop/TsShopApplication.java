99c9435cf52e22c735c2b9bb2beb62545e01a099
tiagosantos-dev <tiagoltj@hotmail.com>
2025-02-03 18:41:59 -0300

    feat(config): iniacialização do projeto

---
+++ src/main/java/com/shopping/ts_shop/TsShopApplication.java
@@ -0,0 +1,13 @@
+package com.shopping.ts_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TsShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TsShopApplication.class, args);
+	}
+
+}
