78f4212b5fcf6d85f640bb9f3e430c641c38d7db
tiagosantos-dev <tiagoltj@hotmail.com>
2025-02-03 18:59:54 -0300

    feat(client): Criando entidade cliente

---
+++ src/main/java/com/shopping/ts_shop/model/Cliente.java
@@ -0,0 +1,20 @@
+package com.shopping.ts_shop.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private String telefone;
+    private String endereco;
+}
