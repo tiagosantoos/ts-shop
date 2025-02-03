1cc598adaefe47b4d9564a1254e26aeb7d4df4ce
tiagosantos-dev <tiagoltj@hotmail.com>
2025-02-03 19:16:23 -0300

    feat(Validacao): Adicionando validação na entidade ItemPedido

--- src/main/java/com/shopping/ts_shop/model/ItemPedido.java
+++ src/main/java/com/shopping/ts_shop/model/ItemPedido.java
@@ -1,6 +1,9 @@
 package com.shopping.ts_shop.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import java.math.BigDecimal;
 
@@ -11,20 +14,28 @@ public class ItemPedido {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotNull(message = "O pedido é obrigatório")
     @ManyToOne
     @JoinColumn(nullable = false)
     private Pedido pedido;
 
+    @NotNull(message = "O produto é obrigatório")
     @ManyToOne
     @JoinColumn(nullable = false)
     private Produto produto;
 
+    @NotNull(message = "A quantidade é obrigatória")
+    @Min(value = 1, message = "A quantidade deve ser pelo menos 1")
     @Column(nullable = false)
     private Integer quantidade;
 
+    @NotNull(message = "O preço unitário é obrigatório")
+    @DecimalMin(value = "0.01", message = "O precoUnitario deve ser maior que zero")
     @Column(nullable = false)
     private BigDecimal precoUnitario;
 
+    @NotNull(message = "O subtotal não pode ser nulo")
+    @DecimalMin(value = "0.01", message = "O subtotal deve ser maior que zero")
     @Column(nullable = false)
     private BigDecimal subtotal;
 }
